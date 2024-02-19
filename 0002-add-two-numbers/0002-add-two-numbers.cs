7102597abb8d9747679e6e177685ecba59b6476c
KMalyshe <91137699+KMalyshe@users.noreply.github.com>
2024-02-19 15:52:36 +0100

    Time: 73 ms (75.50%), Space: 51.2 MB (57.08%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -15,23 +15,29 @@ public class Solution {
         ListNode start = result;
         int carry = 0;
         
-        while(l1 != null || l2 != null){
+        while(l1 != null || l2 != null)
+        {
             int sum = carry;
             
-            if(l1 != null){
+            if(l1 != null)
+            {
                 sum += l1.val;
                 l1 = l1.next;
             }
             
-            if(l2 != null){
+            if(l2 != null)
+            {
                 sum += l2.val;
                 l2 = l2.next;
             }
             
-            if(sum >= 10){
+            if(sum >= 10)
+            {
                 carry = sum / 10;
                 sum = sum % 10;
-            }else{
+            }
+            else
+            {
                 carry = 0;
             }
 
@@ -39,7 +45,8 @@ public class Solution {
             result = result.next;
         }
         
-        if(carry > 0){
+        if(carry > 0)
+        {
             result.next = new ListNode(carry);
         }
         
