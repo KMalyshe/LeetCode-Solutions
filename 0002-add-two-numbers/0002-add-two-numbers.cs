1c305a35210e1b9b4224787a624c83eefdf7aa8b
KMalyshe <91137699+KMalyshe@users.noreply.github.com>
2024-02-19 15:50:22 +0100

    Time: 80 ms (46.04%), Space: 51.3 MB (57.08%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -11,41 +11,38 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        List<int> list1 = getList(l1); List<int> list2 = getList(l2);
-        list1.Reverse(); list2.Reverse();
-        string str1 = ""; string str2 = "";
-        foreach (int num in list1) str1 += num;
-        foreach (int num in list2) str2 += num;
-        str1 = (BigInteger.Parse(str1) + BigInteger.Parse(str2)).ToString();
-        List<int> result = new List<int>();
-        foreach (char num in str1) result.Add((int) Char.GetNumericValue(num));
-        result.Reverse();
-        List<int> removeTrailing0 = result;
-        while ((removeTrailing0.Last() == 0) && (removeTrailing0.Count() > 1)) 
-        {
-            removeTrailing0.RemoveAt(removeTrailing0.Count() - 1);
+        ListNode result = new ListNode(0);
+        ListNode head = result;
+        int carry = 0;
+        
+        while(l1 != null || l2 != null){
+            int sum = carry;
+            
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            if(sum >= 10){
+                carry = sum / 10;
+                sum = sum % 10;
+            }else{
+                carry = 0;
+            }
+
+            result.next = new ListNode(sum);
+            result = result.next;
         }
-        if (removeTrailing0.Count() == 1) return new ListNode(removeTrailing0[0], null);
-        result = removeTrailing0;
-        ListNode node = new ListNode(result[0], new ListNode());
-        ListNode firstnode = node;
-        for (int i = 1; i<result.Count(); i++)
-        {
-            node = node.next;
-            node.val = result[i];
-            if (i != result.Count() - 1) node.next = new ListNode();
+        
+        if(carry > 0){
+            result.next = new ListNode(carry);
         }
-        return firstnode;
-    }
-    
-    public List<int> getList(ListNode node)
-    {
-        List<int> result = new List<int>();
-        while (node != null)
-        {
-            result.Add(node.val);
-            node = node.next;
-        }
-        return result;
+        
+        return head = head.next;
     }
 }
