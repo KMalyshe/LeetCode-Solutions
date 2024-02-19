ba0cd3c19242022785c90d0152a0b357006ca719
KMalyshe <91137699+KMalyshe@users.noreply.github.com>
2024-02-20 00:17:23 +0100

    Time: 116 ms (42.16%), Space: 48.3 MB (38.22%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -2,7 +2,7 @@ public class Solution {
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
         bool[] result = new bool[candies.Length];
         var highest = 0;
-        foreach (int kid in candies) if (kid > highest) highest = kid;
+        foreach (int kid in candies) highest = Math.Max(highest, kid);
         for (int i = 0; i<result.Length; i++) if (candies[i]+extraCandies >= highest) result[i] = true; else result[i] = false;
         return result;
     }
